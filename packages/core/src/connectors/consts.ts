da548ad75ad7513cbcab1eaf84a5ee6b2b40df10
wangsijie <wangsijie@silverhand.io>
2022-07-08 15:23:39 +0800

    feat(core): read connector packages env

--- packages/core/src/connectors/consts.ts
+++ packages/core/src/connectors/consts.ts
@@ -1,16 +1,20 @@
-// TODO: move to environment variable
-export const connectorPackages = [
-  '@logto/connector-alipay',
-  '@logto/connector-alipay-native',
-  '@logto/connector-aliyun-dm',
-  '@logto/connector-aliyun-sms',
-  '@logto/connector-apple',
-  '@logto/connector-facebook',
-  '@logto/connector-github',
-  '@logto/connector-google',
-  '@logto/connector-sendgrid-email',
-  '@logto/connector-smtp',
-  '@logto/connector-twilio-sms',
-  '@logto/connector-wechat',
-  '@logto/connector-wechat-native',
-];
+import { getEnv } from '@silverhand/essentials';
+
+export const connectorPackages = getEnv(
+  'CONNECTOR_PACKAGES',
+  [
+    '@logto/connector-alipay',
+    '@logto/connector-alipay-native',
+    '@logto/connector-aliyun-dm',
+    '@logto/connector-aliyun-sms',
+    '@logto/connector-apple',
+    '@logto/connector-facebook',
+    '@logto/connector-github',
+    '@logto/connector-google',
+    '@logto/connector-sendgrid-email',
+    '@logto/connector-smtp',
+    '@logto/connector-twilio-sms',
+    '@logto/connector-wechat',
+    '@logto/connector-wechat-native',
+  ].join(',')
+).split(',');
